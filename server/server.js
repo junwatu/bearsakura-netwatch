b2c601790e45bf2d08569c0bb618ba2b62fad758
Equan P. <equan.p@gmail.com>
2023-11-02 08:31:13 +0700

    update server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import bodyParser from 'body-parser';
+import axios from 'axios';
 import * as packetCapturer from './packetCapturer.js';
 
 const app = express();
@@ -8,8 +9,21 @@ app.use(bodyParser.json());
 
 const PORT = process.env.PACKET_PORT || 3000;
 const HOST = process.env.PACKET_IP_ADDRESS || 'localhost';
+const SERVER_DB_HOST = process.env.DATABASE_SERVER || 'localhost';
+const SERVER_DB_PORT = process.env.DATABASE_SERVER_PORT || 4000;
 
 packetCapturer.startCapturing(process.env.PACKET_IP_ADDRESS);
+
+setInterval(async () => {
+	const packets = packetCapturer.getPackets();
+	try {
+		await axios.post(`http://${SERVER_DB_HOST}:${SERVER_DB_PORT}/save-packets`, packets);
+		console.log('Packets sent successfully');
+	} catch (error) {
+		console.error('Error sending packets:', error);
+	}
+}, 5000);  // Adjust the interval to your needs
+
 
 app.get('/packets', (req, res) => {
 	res.json(packetCapturer.getPackets());
